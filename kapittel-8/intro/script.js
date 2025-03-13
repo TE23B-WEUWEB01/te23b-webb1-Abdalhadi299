d1ed3b40fe00556fca47be9e88c2ea441e83f14d
Abdalhadi Darkoushe <abdalhadi.darkoushe@elev.ga.ntig.se>
2025-03-13 11:07:23 +0100

    tränat på js, variablar

---
+++ kapittel-8/intro/script.js
@@ -0,0 +1,16 @@
+// var ålder = 18;
+// var tal1 = 12;
+// var tal2 = 24;
+// var summa = tal1 + tal2;
+// alert(summa);
+// alert(tal1 + "+" + tal2 + "=" + summa);
+
+var förnamn = "Abdalhadi";
+var efternamn = "Darkoushe";
+var helaNamnet = förnamn + " " + efternamn;
+var hobby = "min hobbey är att spela fotboll";
+var kostnad = 1000;
+
+var mening = helaNamnet + "S hoppy är " + hobby + ".\n Min hobby kostar " + kostnad + "kr/år.";
+
+alert(mening);
